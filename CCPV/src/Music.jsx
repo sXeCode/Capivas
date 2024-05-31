451ae29c537f9461c353d17ecbf7ae8a289bc8fe
sXeCode <frpcell2019@gmail.com>
2024-05-31 13:01:18 -0300

    Update Music.jsx

--- CCPV/src/Music.jsx
+++ CCPV/src/Music.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Music = () => (
   <section className="Music">
     <h2>Escute Nossa Playlist</h2>
-    <p><a href='https://open.spotify.com/playlist/4mOd30Z69P5k18Qe3RrGP0?si=ea5efe9e48014841'></a></p>
+   <a href="https://open.spotify.com/playlist/4mOd30Z69P5k18Qe3RrGP0?si=ea5efe9e48014841" target='_blank'> <button>Spotfy</button></a>
   </section>
 );
 
