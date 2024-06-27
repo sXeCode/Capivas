97b0d8461ae777def424b2388b7102f08d2044ee
sXeCode <frpcell2019@gmail.com>
2024-06-27 09:18:30 -0300

    a
    a

--- CCPV/src/Header.jsx
+++ CCPV/src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <header className="App-header">
     <h1>Capivandalos</h1>
-    <p>Suport Violent Dancing</p>
+    <p>Support Local Scene </p>
   </header>
 );
 
